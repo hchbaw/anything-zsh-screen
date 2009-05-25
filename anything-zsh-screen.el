ca32105fabbe100eb1f10522d53972573b3a2fe8
Takeshi Banse <takebi@laafc.net>
2009-05-25 23:10:03 +0900

    First exposure.
    Hello World, at last.
    
    Signed-off-by: Takeshi Banse <takebi@laafc.net>

--- anything-zsh-screen.el
+++ anything-zsh-screen.el
@@ -1,51 +1,188 @@
+;;; anything-zsh-screen.el --- zsh completion system with `anything'
+
+;; Copyright (C) 2009 Takeshi Banse <takebi@laafc.net>
+;; Author: Takeshi Banse <takebi@laafc.net>
+;; Keywords: convenience, anything
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; Zsh completion system with Anything interface.
+
+;;; Installation:
+
+;; Put the anything-zsh-screen.el to your load-path.
+;; Put the anything-zsh-screen.zsh (the helper zsh script file) to the
+;; location poited by `anything-zsh-screen-script-file-name'.
+;; (defualt: ~/.emacs.d)
+
+;; Start a screen session named by `anything-zsh-screen-session-name' and
+;; within that session, run the start up commands.
+;; Like below.
+;; % screen -S anything-zsh-screen
+;; % source ~/.emacs.d/anything-zsh-screen.zsh
+;; % anything-zsh-screen-rc
+;; (You can now detach this screen session.)
+
+;; And the following to your ~/.emacs startup file.
+;; (eval-after-load
+;;  'shell
+;;  '(define-key shell-mode-map "\e\t" 'anything-zsh-screen-simple-complete))
+
+;;; Note:
+
+;; This package requires several working executables listed below,
+;; Perl, ruby1.9, zsh and screen.
+
+;;; Code:
+
 (require 'anything)
+(require 'comint)
+(require 'term)
 
-(defvar anything-zsh-screen.zsh
-  (expand-file-name "~/.elisp/anything-zsh-screen.zsh"))
+(defvar anything-zsh-screen-script-file-name
+  (expand-file-name "~/.emacs.d/anything-zsh-screen.zsh")
+  "Location of the installed zsh script file.
+
+Please install the zsh script file (`anything-zsh-screen.zsh' bundled with
+this `anything-zsh-screen' distribution) to this location.")
+(defvar anything-zsh-screen-session-name
+  "anything-zsh-screen"
+  "Session name of the screen to which this elisp program communicates.")
 (defvar anything-zsh-screen-exchange-file-name
-  (expand-file-name "~/tmp/anything-zsh-screen-exchange"))
-(defvar anything-zsh-screen-target nil)
+  (expand-file-name "~/tmp/anything-zsh-screen-exchange")
+  "Location of which will be passed as screen's `hardcopy -h' argument.")
+(defvar anything-zsh-screen-scrollback
+  9999
+  "Number of the lines to be passed as screen's history scrollback buffer.")
+(defvar anything-zsh-screen-zle-line-source-name
+  "command line"
+  "Name of the anything-source which is supposed to be displayed at the zle.")
 
-(defun aczs-get-hardcopy (arg)
-  (when (= (call-process "zsh" nil nil nil
-                         "-c"
-                         (concat
-                          "source " anything-zsh-screen.zsh ";"
-                          "anything-zsh-screen-run \"" arg "\"")) 0)
-    anything-zsh-screen-exchange-file-name))
+(define-anything-type-attribute 'anything-zsh-screen-read
+  '((display-to-real . azs-display-to-real)
+    (action . (("Insert" . azs-insert))))
+  "Anything Zsh Screen Read.")
 
-(defvar anything-zsh-screen-source
-  '((name . "zsh screen")
-    (init
-     . (lambda ()
-         (with-current-buffer (anything-candidate-buffer 'local)
-           (anything-aif (aczs-get-hardcopy anything-zsh-screen-target)
-               (insert-file-contents it)))))
-    (candidates-in-buffer)
-    (action . identity)))
+(defun azs-display-to-real (disp)
+  (let ((target (thing-at-point 'symbol))
+        (sname (assoc-default 'name (anything-get-current-source))))
+    (cond ((and (string-match "option" sname)
+                target
+                (string-match "^-[^-]+" target)) ;; short option
+           (substring disp 1))
+          (t disp))))
 
-(defun anything-zsh-screen ()
+(defun azs-simple-insert-candidate (cand)
+  (let* ((len (azs-get-prefix-length))
+         (del-len (if (< len 0) 0 len)))
+    (delete-region (- (point) del-len) (point))
+    (insert cand)))
+(defun azs-term-insert-candidate (cand)
+  (dotimes (_ (azs-get-prefix-length))
+    (term-send-backspace))
+  (term-send-raw-string cand))
+(defun azs-insert (cand)
+  (let ((insert-func (funcall anything-zsh-screen-get-insert-func)))
+    (funcall insert-func cand)))
+(defvar anything-zsh-screen-get-insert-func
+  (lambda ()
+    (if (eq major-mode 'term-mode)
+      #'azs-term-insert-candidate
+      #'azs-simple-insert-candidate)))
+
+(defvar anything-zsh-screen-complete-target "")
+(defun azs-get-prefix-length ()
+  (let ((sname (assoc-default 'name (anything-get-current-source))))
+    (cond ((string= anything-zsh-screen-zle-line-source-name sname)
+           (length anything-zsh-screen-complete-target))
+          ((string= "directory stack" sname) 1)
+          ((string-match "\\(:?file\\|director\\(:?ies\\|y\\)\\)" sname)
+           (let ((name-maybe
+                  (file-name-nondirectory (thing-at-point 'filename))))
+             (if (string= "" name-maybe)
+               0
+               (length name-maybe))))
+          ((null (thing-at-point 'symbol)) 0)
+          ((string-match "option" sname)
+           (let ((s (thing-at-point 'symbol)))
+             (cond ((string= "-" s) 1)
+                   ((string= "--" s) 2)
+                   (t 0))))
+          (t (length (thing-at-point 'symbol))))))
+
+(defun azs-get-hardcopy (arg chdir)
+  (if (zerop
+       (call-process "zsh" nil nil nil
+                     "-c"
+                     (concat
+                      "source " anything-zsh-screen-script-file-name ";"
+                      (concat
+                       "anything-zsh-screen-run"
+                       " " anything-zsh-screen-exchange-file-name
+                       " " anything-zsh-screen-session-name
+                       " " (number-to-string anything-zsh-screen-scrollback)
+                       " '" arg "' "
+                       " '" anything-zsh-screen-zle-line-source-name "'"
+                       " 'stuff \"cd " chdir "^M\"' "
+                       " 'stuff \"" arg "^I^X^X\"'"))))
+    anything-zsh-screen-exchange-file-name
+    (error "Error while communicating with the screen session")))
+
+(defun azs-read-sources (arg &optional chdir)
+  (anything-aif (azs-get-hardcopy arg (or chdir default-directory))
+      (read (with-temp-buffer
+              (insert-file-contents-literally it)
+              (buffer-string)))))
+
+(defun azs-complete (sources target)
+  (let ((anything-sources sources)
+        (anything-zsh-screen-complete-target target)
+        (anything-execute-action-at-once-if-one t))
+    (anything sources
+              nil
+              "pattern: "
+              nil nil "*anything zsh screen*")))
+
+(defun azs-want-zsh-expander (target)
+  (string-match "{" target))
+(defun azs-complete-cancel-anything ()
+  '(message "Sorry for the inconvenience")
+  (when (eq major-mode 'term-mode)
+    (term-send-raw-string "\C-I")))
+
+(defun anything-zsh-screen-complete (get-target &optional extend-sources)
+  (let ((extend-sources (or extend-sources #'identity))
+        (target (funcall get-target)))
+    (if (azs-want-zsh-expander target)
+      (azs-complete-cancel-anything)
+      (azs-complete (funcall extend-sources (azs-read-sources target))
+                    target))))
+
+(defun anything-zsh-screen-simple-complete ()
   (interactive)
-  (let ((anything-zsh-screen-target anything-pattern))
-    (let ((anything-candidate-number-limit 9999))
-      (anything-nest '(anything-zsh-screen-source)
-                     nil nil nil nil "*anything zsh screen*"))))
+  (anything-zsh-screen-complete (lambda ()
+                                  (buffer-substring-no-properties
+                                   (comint-line-beginning-position)
+                                   (point)))))
 
-(defun aczs-inits (list)
-  (loop for i upto (length list)
-        nconc (list (subseq list 0 i))))
-(defun aczs-tails (list)
-  (loop with size = (length list)
-        for i upto size
-        nconc (list (subseq list i size))))
+(when (require 'anything-show-completion nil t)
+  (use-anything-show-completion 'anything-zsh-screen-complete
+                                '(azs-get-prefix-length)))
 
-(defvar anything-c-current-line-source
-  '((name . "current line")
-    (candidate
-     . (lambda ()
-         (with-current-buffer anything-current-buffer
-           (let ((line0 (buffer-substring-no-properties (point-at-bol)
-                                                        (point))))
-             line0))))))
-             
-             
+(provide 'anything-zsh-screen)
+;;; anything-zsh-screen.el ends here
--- anything-zsh-screen.zsh
+++ anything-zsh-screen.zsh
@@ -1,45 +1,96 @@
-# screen and zsh side's functions.
+## screen and zsh side's functions.
+
+# zle widget.
 function anything-zsh-screen-hardcopy () {
-  local exchange="${HOME}/tmp/anything-zsh-screen-exchange"
   command screen \
     -X eval \
-      "hardcopy -h ${exchange}" \
+      "hardcopy -h ${ANYTHING_ZSH_SCREEN_EXCHANGE}" \
       'scrollback 0' \
+      'stuff "^U"' \
+      'stuff "cd ~/^M"' \
       'stuff "^L"'
 }
 
+# specialized startup command
 function anything-zsh-screen-rc () {
-  LISTMAX=65535
+  ANYTHING_ZSH_SCREEN_EXCHANGE=${1}
+  if [ -z ${ANYTHING_ZSH_SCREEN_EXCHANGE} ]; then
+    ANYTHING_ZSH_SCREEN_EXCHANGE=${HOME}/tmp/anything-zsh-screen-exchange
+  fi
+
+  # formatting and messages
+  zstyle ':completion:*' verbose yes
+  zstyle ':completion:*' list-separator '--'
+  zstyle ':completion:*' group-name ''
   zstyle ':completion:*:descriptions' format '* %d'
+  zstyle ':completion:*:messages' format '%d'
+  zstyle ':completion:*:warnings' format 'No matches for: %d'
+  zstyle ':completion:*:corrections' format '%d (errors: %e)'
+
+  bindkey -e
   zle -N anything-zsh-screen-hardcopy
   bindkey '^X^X' anything-zsh-screen-hardcopy
+  bindkey '^I' complete-word
 
+  LISTMAX=-1
   PS1='> '
 }
 
-# emacs side's functions.
-function anything-zsh-screen-run0 () {
-  command screen -x anything-zsh-screen \
+## shell utility functions.
+function anything-zsh-screen-recompile () {
+  local z=${1}
+  [[ -z ${z} ]] && z=~/.emacs.d/anything-zsh-screen.zsh
+  autoload -U zrecompile
+  [[ -f ${z} ]] && zrecompile -p ${z}
+}
+function anything-zsh-screen-clean () {
+  local z=${1}
+  [[ -z ${z} ]] && z=~/.emacs.d/anything-zsh-screen.zsh.zwc
+  rm -f ${z}
+}
+
+## emacs side's functions.
+function azs-run1 () {
+  local sessionname=${1}
+  local scrollback=${2}
+  shift 2
+
+  command screen -x ${sessionname} \
     -X eval \
       'stuff "^U"' \
-      'stuff "^L"' \
-      'scrollback 65535' \
-      "stuff \"${1}^I^X^X\""
+      "scrollback ${scrollback}" \
+      ${argv}
 }
 
 function anything-zsh-screen-run () {
-  local exchange="${HOME}/tmp/anything-zsh-screen-exchange"
+  local exchange=${1}
+  local sessionname=${2}
+  local scrollback=${3}
+  local elisp_arg=${4}
+  local zle_name=${5}
+  shift 5 # treat rest arguments as the screen commands.
 
   rm -f ${exchange}
-  anything-zsh-screen-run0 "${1}"
+  azs-run1 \
+    ${sessionname} ${scrollback} \
+    ${argv}
+
+  azs-run-process ${elisp_arg} ${zle_name} ${exchange}
+  return $?
+}
+
+function azs-run-process () {
+  local elisp_arg=${1}
+  local zle_name=${2}
+  local exchange=${3}
 
   zmodload zsh/stat
   local ret=-1
   local size=-1
   for _i in {1..20}; do
-    if test -f ${exchange}; then
+    if [[ -f ${exchange} ]]; then
       builtin stat -H h ${exchange}
-      if test $size -eq $h[size]; then
+      if (( ${size} == $h[size] )); then
         ret=0
         break;
       else
@@ -50,25 +101,78 @@ function anything-zsh-screen-run () {
     ret=-1
   done
 
-  if test ${ret} -eq 0; then
+  if (( ${ret} == 0 )); then
+    local capture_start_regexp='^>' # screen side's prompt.
+    [[ -z ${ANYTHING_ZSH_SCREEN_DEBUG} ]] || cp ${exchange} /tmp/azs.debug.tmp1
     perl -w -nl -i \
-      -e "my \$s = \"${1}\";" \
-      -e '$s =~ s/\s+$//;' \
-      -e 'm{^> $s} .. eof and not m/^$/ and print;' \
+      -e "my \$s = \"${capture_start_regexp}\";" \
+      -e 'm{$s} .. eof and not m/^$/ and print;' \
       ${exchange}
-    ruby1.9 -w -nl -i \
+
+    [[ -z ${ANYTHING_ZSH_SCREEN_DEBUG} ]] || cp ${exchange} /tmp/azs.debug.tmp2
+    ruby1.9 --disable-gems \
+      -w -l -i \
       -e  '
-        case $_
-        when /\A>/
-          next
-        when /\A\*/
-          next
-        when / -- /
-          print $_.split(/\s+/).take(1).join
-        else
-          print $_.split(/\s+/).join("\n")
+        ELISP_ARG=ARGV.shift.strip
+        ZLE_NAME=ARGV.shift.strip
+
+        process = ->acc, cs, z, buf, e {
+          case e
+          when /\A>\s+(.+)/
+            buf = $1
+          when /\Acorrections \(errors:/
+            z = e
+          when /\A\*\s+(.+)/
+            if buf != "" and buf != ELISP_ARG
+              v = buf
+              acc << {name:"#{ZLE_NAME}",
+                      cands:[->{ "(#{v.inspect} . #{v.inspect})" }]}
+              buf = ""
+            end
+            if cs.size != 0
+              acc << {name:z, cands:cs}
+            end
+            cs = []
+            z = $1
+          when / -- /
+            cs << ->{
+              if z == "directory stack"
+                "(#{e.inspect} . #{e.split(/ -- /).map(&:strip).last.inspect})"
+              else
+                "(#{e.inspect} . #{e.split(/\s+/).take(1).join().inspect})"
+              end
+            }
+          else
+            if z == "" # continuous parts running through the command line.
+              buf << e
+            else
+              cs << ->{
+                e.split(/\s+/).map {|ee|
+                  "(#{ee.inspect} . #{ee.inspect})"
+                }.join(" ")
+              }
+            end
+          end
+          [acc, cs, z, buf]
+        }
+
+        readlines.map(&:strip).tap {|o|
+          o << "* dummy"
+        }.drop(1).reduce([[], [], "", ""]) {|a, e|
+          process[*a, e]
+        }.tap do |o, *_|
+          s = ""
+          s << "("
+          o.each {|e|
+            s << "((name . #{e[:name].inspect})"
+            s <<  "(candidates . (#{e[:cands].map(&:[]).join(" ")}))"
+            s <<  "(type . anything-zsh-screen-read))"
+          }
+          s << ")"
+
+          puts s
         end
-      ' ${exchange}
+      ' ${elisp_arg} ${zle_name} ${exchange}
     ret=$?
   fi
   return $ret
